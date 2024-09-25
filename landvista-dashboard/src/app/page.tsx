969480095af17d9a14834ce44ed3b52f7be46efa
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-09-25 15:58:48 +0300

    added add users file

--- landvista-dashboard/src/app/page.tsx
+++ landvista-dashboard/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Layout from "./components/Layout";
+import FormPreview from "./components/AddUser";
 
 
 export default function Home() {
  return (
    <Layout>
      <div>
-      
+     <FormPreview/>
    </div>
    </Layout>
   
