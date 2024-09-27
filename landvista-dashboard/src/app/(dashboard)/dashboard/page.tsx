3c930b25957d5d50a1ae47a329a82abefa54c177
Mary Muthoni <muthonimaryvivian@gmail.com>
2024-09-28 00:46:30 +0300

    added splashScreen

--- landvista-dashboard/src/app/(dashboard)/dashboard/page.tsx
+++ landvista-dashboard/src/app/(dashboard)/dashboard/page.tsx
@@ -2,13 +2,14 @@ import Layout from "./components/Layout";
 // import Feedback from "./Feedback/page";
 // import AddUserData from "./add-user-data/page";
 import UserProfile from "./user-profiles/page";
+import SplashScreen from "./teaser";
 
 export default function Home() {
     return (
       <Layout>
         <div>
+          <SplashScreen/>
           <UserProfile/>
-        
       </div>
      </Layout>
      
