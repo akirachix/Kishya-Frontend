176599ef833597a14d6b6af27a057698c24c9c3b
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-09-27 23:15:15 +0300

    made changes

--- landvista-dashboard/src/app/(dashboard)/dashboard/components/Sidebar/index.tsx
+++ landvista-dashboard/src/app/(dashboard)/dashboard/components/Sidebar/index.tsx
@@ -27,7 +27,7 @@ const SidebarNav = () => {
 
         <nav>
           <ul className="space-y-6">
-            <SidebarItem Icon={FaTachometerAlt} label="Dashboard" path="/dashboard//" isActive={false} />
+            <SidebarItem Icon={FaTachometerAlt} label="Dashboard" path="" isActive={false} />
             <SidebarItem Icon={FaBullseye} label="Data Accuracy" path="/ModelAccuracy" isActive={false} />
             <SidebarItem Icon={FaThumbsUp} label="User Feedback" path="/dashboard/Feedback" isActive={false} />
             
