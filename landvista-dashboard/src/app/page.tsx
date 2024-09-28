e63ad506c5f3a6371e99ad642fdb98a965967378
Ynonne-Martin <yvonnemartinkatumbi@gmail.com>
2024-09-28 09:19:09 +0300

    made changes

--- landvista-dashboard/src/app/page.tsx
+++ landvista-dashboard/src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import Layout from "./(web_portal)/web_portal/Layout";
+import Layout from "./(dashboard)/dashboard/components/Layout";
 // import MapDisplay from "./(web_portal)/web_portal/webPortal/components/MapDisplay";
 export default function Home() {
  return (
